9286219fa58ca559feb40deb3bab80f0ce767dd9
Gaurav Tripathi <>
2023-07-02 08:34:06 +0530

    g

---
+++ paidcourse/login.js
@@ -0,0 +1,29 @@
+const form = document.querySelector("form")
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault()
+
+    const username = form.username.value
+    const password = form.password.value
+
+    const authenticated = authentication(username,password)
+
+    if(authenticated){
+        window.location.href = "index.html"
+    }else{
+        alert("wrong")
+    }
+})
+
+// function for checking username and password
+
+function authentication(username,password){
+    if(username === "admin" && password === "password"){
+        return true
+    } else if (username === "Gaurav" && password === "gauravtripathi") {
+        return true
+      } 
+    else{
+        return true
+    }
+}
